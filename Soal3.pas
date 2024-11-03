94f3171d18ca9ffc24b3f4a0c417deb7b464576d
indahmutia <indahmutiatrigana@gmail.com>
2024-11-03 10:06:52 +0700

    Update Soal3.pas

--- Soal3.pas
+++ Soal3.pas
@@ -1,1 +1,29 @@
-//ketik kode jawaban noo 3 kamu disini
+program PolaBintangX;
+var
+  n, i, j: integer;
+begin
+ 
+  write('Masukkan ukuran n: ');
+  readln(n);
+
+  if n mod 2 = 0 then
+  begin
+    writeln('angka tidak valid, harus angka ganjil');
+    readln;
+    exit;
+  end;
+  
+  for i := 1 to n do
+  begin
+    for j := 1 to n do
+    begin
+      if (j = i) or (j = n-i+1) then
+        write('* ')
+      else
+        write('  ');
+    end;
+    writeln;
+  end;
+  
+  readln;
+end.
