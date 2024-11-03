036e927eb3ff4bf7c7bd5a9b071273c62a71f3cb
indahmutia <indahmutiatrigana@gmail.com>
2024-11-03 10:02:38 +0700

    Update Soal1.pas

--- Soal1.pas
+++ Soal1.pas
@@ -1,1 +1,33 @@
-//ketik kode jawaban no 1 kamu disini
+program BiayaTransportSederhana;
+var
+  jarak: real;
+  isPremium: char;
+  biayaDasar, biayaTambahan, totalBiaya: real;
+
+begin
+  
+  write('Jarak (KM): ');
+  readln(jarak);
+  write('Premium? (Y/T): ');
+  readln(isPremium);
+  
+  biayaDasar := 20000;
+  
+  if jarak < 5 then
+    biayaTambahan := jarak * 5000
+  else if jarak <= 10 then
+    biayaTambahan := jarak * 4000
+  else
+    biayaTambahan := jarak * 3000;
+  
+  totalBiaya := biayaDasar + biayaTambahan;
+  
+  if totalBiaya > 100000 then
+  begin
+    totalBiaya := totalBiaya * 0.9;  // Diskon 10%
+    if UpCase(isPremium) = 'Y' then
+      totalBiaya := totalBiaya * 0.95;  // Diskon tambahan 5%
+  end;SS
+  writeln('Total: Rp.', totalBiaya:0:0);
+  readln;
+end.
